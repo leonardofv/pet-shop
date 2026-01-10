5b7e93ad15067f299344df3f28d16546ab945d74
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-01-10 14:39:51 -0300

    feat: fist commit, create project"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
-type props = {
-  title: string;
-};
-
-const Component = ({ title }: props) => {
-  return (
-    <>
-      <h2>Teste component</h2>
-    </>
-  );
-};
-
 export default function Home() {
   return (
     <div>
-      <h2>Hello Next</h2>
-      <Component title={1} />
+      <h2>Hello World!</h2>
     </div>
   );
 }
