3013d7b57194cc5320f264ed3068c5b01f74ffcd
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-01-11 15:59:08 -0300

    teste lefthook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+type props = {
+  title: string;
+};
+
+const Component = ({ title }: props) => {
+  return <h2>Component</h2>;
+};
+
 export default function Home() {
   return (
     <div>
       <h2>Hello World!</h2>
+      <Component title={1} />
     </div>
   );
 }
