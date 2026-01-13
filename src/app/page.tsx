c2063ea6a0d6c753519e30cea64bc972f71124eb
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-01-12 21:20:12 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,102 @@
+import { PeriodSection } from '@/components/period-section';
+import { Appointment as AppointmentPrisma } from '@/generated/prisma/client';
+import {
+  Appointment,
+  AppointmentPeriod,
+  AppointmentPeriodDay,
+} from '@/types/appointment';
+
+const appointments = [
+  {
+    id: '1',
+    petName: 'Romeu',
+    description: 'Consulta',
+    tutorName: 'Erica',
+    phone: '98986854321',
+    scheduleAt: new Date('2026-01-12T17:00:00'),
+  },
+  {
+    id: '2',
+    petName: 'Kely',
+    description: 'Consulta',
+    tutorName: 'Maria',
+    phone: '98986854321',
+    scheduleAt: new Date('2026-01-13T11:00:00'),
+  },
+];
+
+const getPeriod = (hour: number): AppointmentPeriodDay => {
+  if (hour >= 9 && hour < 12) return 'morning';
+  if (hour >= 13 && hour < 18) return 'afternoon';
+  return 'evening';
+};
+
+//função retorna um array de AppointmentPeriod
+function groupAppointmentByPeriod(
+  appointments: AppointmentPrisma[]
+): AppointmentPeriod[] {
+  const transformedAppointments: Appointment[] = appointments?.map((apt) => ({
+    ...apt,
+    time: apt.scheduleAt.toLocaleTimeString('pt-Br', {
+      hour: '2-digit',
+      minute: '2-digit',
+    }),
+    service: apt.description,
+    period: getPeriod(apt.scheduleAt.getHours()),
+  }));
+
+  const morningAppointments = transformedAppointments.filter(
+    (apt) => apt.period === 'morning'
+  );
+  const afternoonAppointments = transformedAppointments.filter(
+    (apt) => apt.period === 'afternoon'
+  );
+  const evenongAppointments = transformedAppointments.filter(
+    (apt) => apt.period === 'evening'
+  );
+
+  return [
+    {
+      title: 'Manhã',
+      type: 'morning',
+      timeRange: '09h-12h',
+      appointments: morningAppointments,
+    },
+    {
+      title: 'Tarde',
+      type: 'afternoon',
+      timeRange: '13h-18h',
+      appointments: afternoonAppointments,
+    },
+    {
+      title: 'Noite',
+      type: 'evening',
+      timeRange: '19h-21h',
+      appointments: evenongAppointments,
+    },
+  ];
+}
+
 export default function Home() {
+  const periods = groupAppointmentByPeriod(appointments);
+
   return (
-    <div>
-      <h2>Hello World!</h2>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between md:m-8">
+        <div>
+          <h1 className="text-title-size text-content-primary mb-2">
+            Sua Agenda
+          </h1>
+          <p className="text-paragraph-medium-size text-content-secondary">
+            Aqui você pode ver todos os clientes e serviços agendados para hoje.
+          </p>
+        </div>
+      </div>
+      <div>
+        {periods.map((period, index) => (
+          <PeriodSection period={period} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
