e5500f6000573b20e30ece5798cb60d55b164732
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-01-29 16:18:52 -0300

    starting form in appointment-form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import { AppointmentForm } from '@/components/appointment-form/appointment-form';
 import { PeriodSection } from '@/components/period-section';
-// import { prisma } from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 import { APPOINTMENT_DATA, groupAppointmentByPeriod } from '@/utils';
 
 export default async function Home() {
@@ -24,6 +25,9 @@ export default async function Home() {
         {periods.map((period, index) => (
           <PeriodSection period={period} key={index} />
         ))}
+      </div>
+      <div className="bg-[#23242C] fixed bottom-0 right-0 left-0 flex justify-center py-4.5 px-6 md:bottom-6 md:right-6 md:left-auto md:top-auto md:w-auto md:bg-transparent md:p-0">
+        <AppointmentForm />
       </div>
     </div>
   );
---
+++ src/components/appointment-form/appointment-form.tsx
@@ -0,0 +1,148 @@
+'use client';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Dog, User } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+
+const appointmentFormSchema = z.object({
+  tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
+  petName: z.string().min(3, 'O nome do pet é obrigatório'),
+  // phone: z.string().min(11, 'O nome do tutor é obrigatório'),
+  description: z.string().min(3, 'uma descrição é obrigatório'),
+});
+
+type AppointmentFormValues = z.infer<typeof appointmentFormSchema>;
+
+export const AppointmentForm = () => {
+  const form = useForm<AppointmentFormValues>({
+    resolver: zodResolver(appointmentFormSchema),
+    defaultValues: {
+      tutorName: ' ',
+      petName: ' ',
+      // phone: ' ',
+      description: ' ',
+    },
+  });
+
+  const onSubmit = (data: AppointmentFormValues) => {
+    console.log(data);
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={'brand'}>Novo Agendamento</Button>
+      </DialogTrigger>
+
+      <DialogContent
+        variant={'appointment'}
+        overlayVariant={'blurred'}
+        showCloseButton
+      >
+        <DialogHeader>
+          <DialogTitle size={'modal'}>Agende um atendimento</DialogTitle>
+          <DialogDescription size={'modal'}>
+            Preenha os dados do cliente para realizar o agendamento:
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            {/* tutorName */}
+            <FormField
+              control={form.control}
+              name="tutorName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Nome do tutor
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <User
+                        className="absolute left-3 top-1/2 -translate-y-1/2 transform text-content-brand"
+                        size={20}
+                      />
+                      <Input
+                        placeholder="Nome do tutor"
+                        className="pl-10"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* petName */}
+            <FormField
+              control={form.control}
+              name="petName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Nome do pet
+                  </FormLabel>
+                  <FormControl>
+                    <div className="relative">
+                      <Dog
+                        className="absolute left-3 top-1/2 -translate-y-1/2 transform text-content-brand"
+                        size={20}
+                      />
+                      <Input
+                        placeholder="Nome do pet"
+                        className="pl-10"
+                        {...field}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage /> {/* mostrar mensagem do zod */}
+                </FormItem>
+              )}
+            />
+            {/* description */}
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Descrição do serviço
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="descrição do serviço"
+                      className="resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage /> {/* mostrar mensagem do zod */}
+                </FormItem>
+              )}
+            />
+
+            <button type="submit">Salvar</button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
