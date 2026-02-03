2f7eeea60fd1d2b669088217f75a64b859a443d8
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-02-03 15:30:31 -0300

    creating data in form

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -8,7 +8,7 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
-import z from 'zod';
+import z, { number } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import {
@@ -19,28 +19,58 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form';
-import { CalendarIcon, ChevronDownIcon, Dog, Phone, User } from 'lucide-react';
+import {
+  CalendarIcon,
+  ChevronDownIcon,
+  Clock,
+  Dog,
+  Phone,
+  User,
+} from 'lucide-react';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { IMaskInput } from 'react-imask';
-import { format, startOfToday } from 'date-fns';
+import { format, setHours, setMinutes, startOfToday } from 'date-fns';
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { cn } from '@/lib/utils';
 import { Calendar } from '../ui/calendar';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select';
 
-const appointmentFormSchema = z.object({
-  tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
-  petName: z.string().min(3, 'O nome do pet é obrigatório'),
-  phone: z.string().min(11, 'O nome do tutor é obrigatório'),
-  description: z.string().min(3, 'uma descrição é obrigatório'),
-  scheduleAt: z
-    .date({
-      error: 'A data é obrigatória',
-    })
-    .min(startOfToday(), {
-      message: 'A data não pode ser inferior a data de hoje',
-    }),
-});
+const appointmentFormSchema = z
+  .object({
+    tutorName: z.string().min(3, 'O nome do tutor é obrigatório'),
+    petName: z.string().min(3, 'O nome do pet é obrigatório'),
+    phone: z.string().min(11, 'O nome do tutor é obrigatório'),
+    description: z.string().min(3, 'uma descrição é obrigatório'),
+    scheduleAt: z
+      .date({
+        error: 'A data é obrigatória',
+      })
+      .min(startOfToday(), {
+        message: 'A data não pode ser inferior a data de hoje',
+      }),
+    time: z.string().min(1, 'A hora é obrigatória'),
+  })
+  .refine(
+    (data) => {
+      const [hour, minute] = data.time.split(':');
+      const scheduleDateTime = setMinutes(
+        setHours(data.scheduleAt, Number(hour)),
+        Number(minute)
+      );
+      return scheduleDateTime > new Date(); //montando a data para checar se a data é maior que o atual
+    },
+    {
+      path: ['time'],
+      error: 'O horário não pode ser no passado',
+    }
+  );
 
 type AppointmentFormValues = z.infer<typeof appointmentFormSchema>;
 
@@ -227,6 +257,34 @@ export const AppointmentForm = () => {
               )}
             />
 
+            <FormField
+              control={form.control}
+              name="time"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-label-medium-size text-content-primary">
+                    Hora
+                  </FormLabel>
+                  <FormControl>
+                    <Select onValueChange={field.onChange} value={field.value}>
+                      <SelectTrigger>
+                        <div className="flex items-center gap-2">
+                          <Clock className="h-4 w-4 text-content-brand" />
+                          <SelectValue placeholder="--:-- --" />
+                        </div>
+                      </SelectTrigger>
+                      <SelectContent>
+                        {TIME_OPTIONS.map((time) => (
+                          <SelectItem value={time}>{time}</SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage /> {/* mostrar mensagem do zod */}
+                </FormItem>
+              )}
+            />
+
             <button type="submit">Salvar</button>
           </form>
         </Form>
@@ -234,3 +292,22 @@ export const AppointmentForm = () => {
     </Dialog>
   );
 };
+
+/**
+ 9:00
+ 9:30
+ 10:00
+ */
+const generateTimeOptions = (): string[] => {
+  const times = [];
+  for (let hour = 9; hour <= 21; hour++) {
+    for (let minute = 0; minute < 60; minute += 30) {
+      if (hour === 21 && minute > 0) break;
+      const timeString = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+      times.push(timeString);
+    }
+  }
+  return times;
+};
+
+const TIME_OPTIONS = generateTimeOptions();
