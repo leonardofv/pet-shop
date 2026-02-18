950a6203a8285c14e4c6596f70ba637a9bd08977
leonardo_fonteles <fonteles.leonardo@gmail.com>
2026-02-18 16:30:45 -0300

    added calendar and datePicker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
 import { AppointmentForm } from '@/components/appointment-form/appointment-form';
+import { DatePicker } from '@/components/date-picker';
 import { PeriodSection } from '@/components/period-section';
 import { Button } from '@/components/ui/button';
 import { prisma } from '@/lib/prisma';
 import { groupAppointmentByPeriod } from '@/utils';
+import { endOfDay, parseISO, startOfDay } from 'date-fns';
 
-export default async function Home() {
-  const appointments = await prisma.appointment.findMany();
-  console.log('Teste', appointments);
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ date?: string }>;
+}) {
+  const { date } = await searchParams;
+  // console.log(date);
+  const selectedDate = date ? parseISO(date) : new Date();
+
+  const appointments = await prisma.appointment.findMany({
+    where: {
+      scheduleAt: {
+        gte: startOfDay(selectedDate), //filtrar agendamentos
+        lte: endOfDay(selectedDate),
+      },
+    },
+    orderBy: {
+      scheduleAt: 'asc', //ordenar datas de agendamentos
+    },
+  });
+
+  // console.log('Teste', appointments);
 
   const periods = groupAppointmentByPeriod(appointments);
 
@@ -21,7 +42,16 @@ export default async function Home() {
             Aqui você pode ver todos os clientes e serviços agendados para hoje.
           </p>
         </div>
+        {/* dapePicker em telas maiores */}
+        <div className="hidden md:flex items-center gap-4">
+          <DatePicker />
+        </div>
       </div>
+      {/* datePicker em telas mobile */}
+      <div className="mt-3 mb-8 md:hidden">
+        <DatePicker />
+      </div>
+
       <div>
         {periods.map((period, index) => (
           <PeriodSection period={period} key={index} />
